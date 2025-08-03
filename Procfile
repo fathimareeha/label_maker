d51588a327c3878d600f83bb55461dae13ad5c87
fathima reeha <reehavoc@gmail.com>
2025-08-03 21:03:38 +0530

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn shipping_label_maker.wsgi
--- shipping_label_maker/settings.py
+++ shipping_label_maker/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,11 +81,11 @@ WSGI_APPLICATION = 'shipping_label_maker.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'label_db',
-        'HOST':'localhost',
-        'PORT':'5432',
-        'USER':'postgres',
-        'PASSWORD':'reeha@1'
+       'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
